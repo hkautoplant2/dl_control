120401ebab8ea26c08f7b48b4d65c682fb43a666
jetson <hkautoplant2@gmail.com>
2020-11-21 17:24:38 +0100

    Code and network runs from ROS node on the jetson, log result is a map on host computer

--- src/aaashell.sh
+++ src/aaashell.sh
@@ -2,17 +2,45 @@
 # This is a comment!
 echo Start communicating to HOST	
 
-#scp foo.txt autoplant2@192.168.1.102:~/
+#Clean up inference result map on jetson
+rm ~/res_inf_jet/*
 
+#Copy zed image into host locally
+scp -r ~/run_inf_jet/* autoplant2@192.168.1.102:~/inference/run_inf_host
 
-#ssh root@540776e7638c 'tlt-infer faster_rcnn -e /workspace/examples/faster_rcnn/specs/fastrcnn_retrain_pruned12.txt'
+#Copy zed image from host into container
+ssh autoplant2@192.168.1.102 'docker cp inference/run_inf_host/. e3a86180333e:/workspace/run_inf_cont'
 
-scp /home/jetson/Downloads/test_image2.png autoplant2@192.168.1.102:~/
-
-ssh autoplant2@192.168.1.102 'docker cp test_image2.png pedantic_chatelet:/workspace/tlt-experiments/data/Test/images'
-
+#Execute inference on the image
 ssh autoplant2@192.168.1.102 'docker exec -i e3a86180333e tlt-infer faster_rcnn -e /workspace/examples/faster_rcnn/specs/fastrcnn_retrain_pruned12.txt'
 
-ssh autoplant2@192.168.1.102 'docker cp pedantic_chatelet:/workspace/tlt-experiments/data/faster_rcnn/inference_results_imgs_retrain/test_image2.png ~/inference'
+#Copy resulting labeled image and label file to local host map 
+ssh autoplant2@192.168.1.102 'docker cp e3a86180333e:/workspace/res_inf_cont/. ~/inference/res_inf_host'
 
-ssh autoplant2@192.168.1.102 'docker cp pedantic_chatelet:/workspace/tlt-experiments/data/faster_rcnn/inference_dump_labels_retrain/test_image2.txt ~/inference'
+#Copy resulting labeled image and label file into inference log map
+ssh autoplant2@192.168.1.102 'cp -r ~/inference/res_inf_host/. ~/inference/log_inf_host'
+
+#Copy results from host to jetson map
+scp -r autoplant2@192.168.1.102:~/inference/res_inf_host/* ~/res_inf_jet
+
+#Clean run inference folder for network
+ssh autoplant2@192.168.1.102 'docker exec -i e3a86180333e rm -rf run_inf_cont'
+
+#Clean inference result map for network
+ssh autoplant2@192.168.1.102 'docker exec -i e3a86180333e rm -rf res_inf_cont'
+
+#Create the removed directories again (ugly solution)
+ssh autoplant2@192.168.1.102 'docker exec -i e3a86180333e mkdir run_inf_cont'
+ssh autoplant2@192.168.1.102 'docker exec -i e3a86180333e mkdir res_inf_cont'
+
+#Clean up host run inference map
+ssh autoplant2@192.168.1.102 'rm inference/run_inf_host/*'
+
+#Clean up host resulting inference map
+ssh autoplant2@192.168.1.102 'rm inference/res_inf_host/*'
+
+#Clean up jetsons run inference maps
+rm ~/run_inf_jet/*
+
+
+
--- src/shelltest.py
+++ src/shelltest.py
@@ -8,29 +8,116 @@ from std_msgs.msg import Float32MultiArray
 from std_msgs.msg import Bool
 import time
 import subprocess
+import os
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+import datetime
 
+right_pos = False
 
 def callback(data):
     #rospy.loginfo('fake_arm_ctrl -> coord_3D callback heard coordinates: %s ', data.data)
     x=1
 
+def show(pic_data):
 
+
+        bridge = CvBridge()
+        cv_image = bridge.imgmsg_to_cv2(pic_data, "bgr8")
+        #directory = r'C:\Users\Rajnish\Desktop\GeeksforGeeks'
+        #os.chdir(directory)
+        basename = "mylogfile"
+        suffix = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
+        filename = "_".join([basename, suffix]) # e.g. 'mylogfile_120508_171442'
+        filename = filename + '.png'
+        path = os.getcwd() + '/' + filename
+        #print(filename)
+
+        print('save image')
+        cv2.imwrite(filename, cv_image)
+        print('image saved')
+        subprocess.Popen(["bash", "/home/jetson/catkin_ws/src/dl_control/src/aaashell.sh"])
+        time.sleep(60)
+        print('done sleeping')
+
+        #img = cv2.imread(path)
+        #cv2.imshow("Image window", img)
+        
+       
+        #cv2.waitKey(1)
+
+def callback_bool(data):
+    #rospy.loginfo('image_to_dnn -> arm_BP callback heard:  %s ', data.data)
+
+
+    global right_pos
+
+    if data.data==True:
+      right_pos = True
+    else:
+      right_pos = False
+    return right_pos
+
+
+
+def callback2(data):
+    #print(data.header)
+    
+    
+    
+
+    if len(data.data)==3686400 and right_pos == True:
+        print('if')
+        #print(data.header)
+        show(data)
+        print('image done')
+        
+        #time.sleep(2)
+
+        '''bridge = CvBridge()
+        cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+        #directory = r'C:\Users\Rajnish\Desktop\GeeksforGeeks'
+        #os.chdir(directory)
+        filename = 'image.png'
+        path = os.getcwd() + '/' + filename
+
+        cv2.imwrite(filename, cv_image)
+        img = cv2.imread(path)
+        cv2.imshow("Image window", img)
+        #cv2.waitKey(1)
+        #time.sleep(2)'''
+        
+            
+    
     
 
 def main():
     rospy.init_node('shell_test', anonymous=False)
+    directory = '/home/jetson/run_inf_jet'
+    os.chdir(directory)
 
 
 
     rospy.loginfo('----------test shell script-----------')
+    image_sub = rospy.Subscriber('/zed2/zed_node/left/image_rect_color',Image,callback2)
+    bp_sub = rospy.Subscriber("arm_BP", Bool, callback_bool)
    
 
-    while not rospy.is_shutdown():
+    #while not rospy.is_shutdown():
         
         
-        print('Testing')
-        subprocess.Popen(["bash", "aaashell.sh"])
-        time.sleep(60)
+        #print('Testing')
+        #time.sleep(2)
+        #print('yo')
+        #print(os.getcwd())
+        #callback2()
+        
+
+
+
+        #subprocess.Popen(["bash", "aaashell.sh"])
+        
 
 #process = subprocess.Popen(['bash', '/aaashell.sh'],
 #                     stdout=subprocess.PIPE, 
@@ -39,7 +126,7 @@ def main():
 #stdout, stderr = process.communicate()
         
    
-   #rospy.spin()
+    rospy.spin()
 
 
 if __name__ == '__main__':
